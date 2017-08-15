7c30aec12fdaf75c621fefa7252a365acadee95e
Dylan Bourdere <loperanes@gmail.com>
2017-08-16 01:36:46 +0200

    Refactoring

--- app/App.php
+++ app/App.php
@@ -2,32 +2,14 @@
 namespace App; 
 class App{
 
-    const DB_NAME = 'blog';
-    const DB_USER = 'root';
-    const DB_PASS = '';
-    const DB_HOST = 'localhost';
+    public $title = "Blog";
+    private static $_instance;
 
-    private static $database;
-    private static $title = 'Blog';
-
-    public static function getDb(){
-        if( self::$database === null )
-        {
-            self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    public static function getInstance(){
+        if(self::$_instance === null){
+            self::$_instance = new App();
         }
-        return self::$database;
+        return self::$_instance;
     }
 
-    public static function notFound(){
-        header("HTTP/1.0 404 Not Found");
-        header('Location:index.php?p=404');
-    }
-
-    public static function getTitle(){
-        return self::$title;
-    }
-
-    public static function setTitle($title){
-        self::$title = $title;
-    }
 }
--- public/index.php
+++ public/index.php
@@ -3,25 +3,6 @@
 require '../app/Autoloader.php';
 App\Autoloader::register();
 
-if(isset($_GET['p'])){
-    $p = $_GET['p'];
-}else {
-    $p = 'home';
-}
 
-//Init
-
-$db = new App\Database('blog');
-
-
-ob_start();
-if($p === 'home'){
-    require '../pages/home.php';
-}elseif($p === 'article'){
-    require '../pages/single.php';
-}elseif($p === 'categorie'){
-    require '../pages/categorie.php';
-}
-$content = ob_get_clean();
-require '../pages/templates/default.php';
+var_dump(App\Config::getInstance()->get('db_user'));
 ?>
